9796f02b62a939d990ca78b5ab8a88c12783df4b
jpli02 <jplee0202@gmail.com>
2025-07-14 09:45:09 -0500

    start

--- realhf/api/cli_args.py
+++ realhf/api/cli_args.py
@@ -872,7 +872,7 @@ class TensorBoardConfig:
 
 def get_user_tmp():
     user = getpass.getuser()
-    user_tmp = os.path.join("/home", user, ".cache", "realhf")
+    user_tmp = os.path.join("/u", user, ".cache", "realhf")
     os.makedirs(user_tmp, exist_ok=True)
     return user_tmp
 
